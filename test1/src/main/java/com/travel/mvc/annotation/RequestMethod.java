92d0c99210f677c926879ec2f72ee69acca9ca83
sungsookim <kimsungsoo0204@daum.net>
2024-12-10 19:16:35 +0900

    ㅎ

--- test1/src/main/java/com/travel/mvc/annotation/RequestMethod.java
+++
@@ -1,5 +0,0 @@
-package com.travel.mvc.annotation;
-
-public enum RequestMethod {
-	GET, POST, HEAD, PUT, PATCH, DELETE, OPTIONS, TRACE
-}
--- test1/src/main/java/com/travel/util/MyMultipartFile.java
+++
@@ -1,7 +0,0 @@
-package com.travel.util;
-
-public interface MyMultipartFile {
-	public String getOriginalFilename();
-	public String getSaveFilename();
-	public long getSize();
-}
